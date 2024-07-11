0b5cf0a6020c07f5b6f781afb369773796157c44
AndreaBoiko <andriy.boyko35@gmail.com>
2024-07-12 01:46:54 +0200

    logo return scroll back to the home page welcoming block after clicking

--- src/containers/logo/Logo.tsx
+++ src/containers/logo/Logo.tsx
@@ -3,18 +3,22 @@ import logo from '~/assets/logo.svg'
 import logoLight from '~/assets/logo-light.svg'
 import Box, { BoxProps } from '@mui/material/Box'
 import { ComponentEnum } from '~/types'
+import HashLink from '~/components/hash-link/HashLink'
+import { guestRoutes } from '~/router/constants/guestRoutes'
 
 interface LogoProps extends BoxProps {
   light?: boolean
 }
 
 const Logo: FC<LogoProps> = ({ light = false, ...props }) => (
-  <Box
-    alt='logo'
-    component={ComponentEnum.Img}
-    src={light ? logoLight : logo}
-    {...props}
-  />
+  <HashLink to={guestRoutes.welcome.path}>
+    <Box
+      alt='logo'
+      component={ComponentEnum.Img}
+      src={light ? logoLight : logo}
+      {...props}
+    />
+  </HashLink>
 )
 
 export default Logo
