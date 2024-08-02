0e3678b6b22805aac6263262e119e6aa9d40846c
Alex <alex.fiendish@gmail.com>
2024-08-02 16:20:30 +0300

    added styles and functionality with mocked data

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,13 +1,68 @@
-import Box from '@mui/material/Box'
-
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Box, Typography, Autocomplete, TextField } from '@mui/material'
+import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
+import img from '~/assets/img/tutor-home-page/become-tutor/subjects.svg'
 
 const SubjectsStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+  const [categoryInputValue, setCategoryInputValue] = useState('')
+  const [categoryValue, setCategoryValue] = useState(null)
+  const [subjectInputValue, setSubjectInputValue] = useState('')
+  const [subjectValue, setSubjectValue] = useState(null)
+
+  const handleClick = () => {}
+
   return (
     <Box sx={styles.container}>
+      <Box sx={styles.imgContainer}>
+        <Box component='img' src={img} sx={styles.img} />
+      </Box>
       <Box sx={styles.rigthBox}>
-        Subjects step
-        {btnsBox}
+        <Typography sx={styles.typography}>
+          {t(
+            'Please choose the main subjects based on the category. You can add others later.'
+          )}
+        </Typography>
+        <Box sx={styles.mobileImgContainer}>
+          <Box component='img' src={img} sx={styles.img} />
+        </Box>
+        <Autocomplete
+          inputValue={categoryInputValue}
+          onChange={(event, newValue) => setCategoryValue(newValue)}
+          onInputChange={(event, newInputValue) =>
+            setCategoryInputValue(newInputValue)
+          }
+          options={['cat1', 'cat2', 'cat3', 'cat4']}
+          renderInput={(params) => (
+            <TextField {...params} label={t('Main Tutoring Category')} />
+          )}
+          value={categoryValue}
+        />
+
+        <Autocomplete
+          inputValue={subjectInputValue}
+          onChange={(event, newValue) => setSubjectValue(newValue)}
+          onInputChange={(event, newInputValue) =>
+            setSubjectInputValue(newInputValue)
+          }
+          options={['subject1', 'subject2', 'subject3', 'subject4']}
+          renderInput={(params) => (
+            <TextField {...params} label={t('Subject')} />
+          )}
+          value={subjectValue}
+        />
+
+        <AppButton
+          onClick={handleClick}
+          size='large'
+          sx={styles.appButton}
+          variant='tonal'
+        >
+          {t('Add one more subject')}
+        </AppButton>
+        <Box sx={styles.btnsBlock}>{btnsBox}</Box>
       </Box>
     </Box>
   )
--- src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
@@ -8,5 +8,30 @@ export const styles = {
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0px' },
     ...fadeAnimation
+  },
+  img: {
+    width: '100%'
+  },
+  imgContainer: {
+    display: { md: 'block', sm: 'none', xs: 'none' }
+  },
+  mobileImgContainer: {
+    display: { md: 'none', sm: 'block' },
+    maxWidth: { sm: '250px', xs: '200px' },
+    m: { sm: '0 auto', xs: '0 auto' }
+  },
+  typography: {
+    fontSize: { md: '16px', xs: '14px' }
+  },
+  rigthBox: {
+    maxWidth: '432px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '14px',
+    m: { md: 0, xs: '0 auto' },
+    pt: 0
+  },
+  btnsBlock: {
+    mt: 'auto'
   }
 }
