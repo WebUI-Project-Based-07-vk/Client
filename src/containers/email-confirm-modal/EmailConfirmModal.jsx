722b0ac74576a3efa3404a4c491e6447c7aed206
sachkova_yuliia <sachkova_yuliia@gmail.com>
2024-08-05 15:27:17 +0200

    modified files for email confirmation pop-up

--- src/containers/email-confirm-modal/EmailConfirmModal.jsx
+++ src/containers/email-confirm-modal/EmailConfirmModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import { styles } from '~/containers/email-confirm-modal/EmailConfirmModal.styles'
@@ -5,6 +6,7 @@ import { useCallback } from 'react'
 import { useModalContext } from '~/context/modal-context'
 import { useTranslation } from 'react-i18next'
 import imgReject from '~/assets/img/email-confirmation-modals/not-success-icon.svg'
+import imgSucces from '~/assets/img/email-confirmation-modals/success-icon.svg'
 import LoginDialog from '~/containers/guest-home-page/login-dialog/LoginDialog'
 import useAxios from '~/hooks/use-axios'
 import { AuthService } from '~/services/auth-service'
@@ -33,44 +35,50 @@ const EmailConfirmModal = ({ confirmToken, openModal }) => {
     return <Loader size={100} />
   }
 
+  let title, description, img, buttonTitle, buttonAction
+
   if (
     (error && error.code === 'BAD_CONFIRM_TOKEN') ||
     (error && error.code === 'DOCUMENT_NOT_FOUND' && response === null)
-  ) {
-    return (
-      <Box sx={styles.box}>
-        <ImgTitleDescription
-          description={t('modals.emailReject.badToken')}
-          img={imgReject}
-          style={styles}
-          title={t('modals.emailNotConfirm')}
-        />
-        <Button onClick={closeModal} sx={styles.button} variant='contained'>
-          {t('common.confirmButton')}
-        </Button>
-      </Box>
-    )
+  ) { 
+      img = imgReject
+      title = t('modals.emailNotConfirm')
+      description = t('modals.emailReject.badToken')
+      buttonTitle = t('common.confirmButton') 
+      buttonAction = closeModal
+  } else if (error && error.code === 'EMAIL_ALREADY_CONFIRMED') {
+      img = imgReject  
+      title = t('modals.emailAlreadyConfirm')
+      description = t('modals.emailReject.alreadyConfirmed')
+      buttonTitle = t('common.confirmButton') 
+      buttonAction = openLoginDialog
+  } else if (response) {
+      img = imgSucces
+      title = t('modals.emailConfirm')
+      description = ''
+      buttonTitle = t('button.goToLogin')
+      buttonAction = () => {
+        closeModal()
+        window.location.href = `${process.env.CLIENT_URL}`
+      }
   }
-
-  if (error && error.code === 'EMAIL_ALREADY_CONFIRMED') {
     return (
-      <Box sx={styles.box}>
+      <Box sx={styles.modal}>
         <ImgTitleDescription
-          description={t('modals.emailReject.alreadyConfirmed')}
-          img={imgReject}
-          style={styles}
-          title={t('modals.emailAlreadyConfirm')}
+          img={img}
+          description={description}
+          title={title}
         />
         <Button
-          onClick={openLoginDialog}
+          onClick={buttonAction}
           sx={styles.button}
           variant='contained'
         >
-          {t('common.confirmButton')}
+          {buttonTitle}
         </Button>
       </Box>
     )
-  }
 }
 
 export default EmailConfirmModal
+/* eslint-enable */
