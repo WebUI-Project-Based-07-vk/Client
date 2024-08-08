8ccbb6916f16282463c591073767c4e32802e614
Oleh Pona <olehpona@gmail.com>
2024-08-08 18:03:41 +0300

    chips implemented

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Box, Typography, Autocomplete, TextField } from '@mui/material'
 import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
 import img from '~/assets/img/tutor-home-page/become-tutor/subjects.svg'
+import { useStepContext } from '~/context/step-context'
+import AppPopover from '~/components/app-popover/AppPopover'
+import AppChip from '~/components/app-chip/AppChip'
 
 const SubjectsStep = ({ btnsBox }) => {
   const { t } = useTranslation()
@@ -11,8 +14,23 @@ const SubjectsStep = ({ btnsBox }) => {
   const [categoryValue, setCategoryValue] = useState(null)
   const [subjectInputValue, setSubjectInputValue] = useState('')
   const [subjectValue, setSubjectValue] = useState(null)
+  const {
+    handleStepData,
+    stepData: { subjects }
+  } = useStepContext()
+  const containerRef = useRef(null)
+  const [containerSize, setContainerSize] = useState({ width: 0, height: 0 })
 
-  const handleClick = () => {}
+  useEffect(() => {
+    if (containerRef.current) {
+      const { offsetWidth, offsetHeight } = containerRef.current
+      setContainerSize({ width: offsetWidth, height: offsetHeight })
+    }
+  }, [])
+
+  const handleClick = () => {
+    subjectValue && handleStepData('subjects', [...subjects, subjectValue])
+  }
 
   return (
     <Box sx={styles.container}>
@@ -30,7 +48,10 @@ const SubjectsStep = ({ btnsBox }) => {
         </Box>
         <Autocomplete
           inputValue={categoryInputValue}
-          onChange={(event, newValue) => setCategoryValue(newValue)}
+          onChange={(event, newValue) => {
+            setCategoryValue(newValue)
+            setSubjectValue(null)
+          }}
           onInputChange={(event, newInputValue) =>
             setCategoryInputValue(newInputValue)
           }
@@ -42,6 +63,7 @@ const SubjectsStep = ({ btnsBox }) => {
         />
 
         <Autocomplete
+          disabled={!categoryValue}
           inputValue={subjectInputValue}
           onChange={(event, newValue) => setSubjectValue(newValue)}
           onInputChange={(event, newInputValue) =>
@@ -53,15 +75,38 @@ const SubjectsStep = ({ btnsBox }) => {
           )}
           value={subjectValue}
         />
-
-        <AppButton
-          onClick={handleClick}
-          size='large'
-          sx={styles.appButton}
-          variant='tonal'
-        >
+        <AppButton onClick={handleClick} size='large' variant='tonal'>
           {t('Add one more subject')}
         </AppButton>
+
+        <Box ref={containerRef} sx={styles.chipPopup}>
+          {subjects.length > 0 && (
+            <AppPopover
+              initialItems={subjects.slice(0, 4).map((e) => (
+                <AppChip key={e}>{e}</AppChip>
+              ))}
+              initialItemsWrapperStyle={styles.initialChips}
+              showMoreElem={
+                subjects.slice(4).length > 0 && (
+                  <AppChip>{`+${subjects.slice(4).length}`}</AppChip>
+                )
+              }
+            >
+              <Box
+                sx={{
+                  ...styles.initialChips,
+                  width: containerSize.width,
+                  maxHeight: containerSize.height,
+                  p: '3px'
+                }}
+              >
+                {subjects.slice(4).map((e) => (
+                  <AppChip key={e}>{e}</AppChip>
+                ))}
+              </Box>
+            </AppPopover>
+          )}
+        </Box>
         <Box sx={styles.btnsBlock}>{btnsBox}</Box>
       </Box>
     </Box>
--- src/types/components/step-context/step-context.types.ts
+++ src/types/components/step-context/step-context.types.ts
@@ -1,11 +1,22 @@
-type stepDataType = {
-  generalInfo: []
-  subjects: []
-  language: []
-  photo: string
+type GeneralInfoType = []
+type SubjectsType = string[]
+type LanguageType = []
+
+type PhotoType = {
+  fileName: string
+  image: string
 }
 
+type StepDataType = {
+  generalInfo: GeneralInfoType
+  subjects: SubjectsType
+  language: LanguageType
+  photo: PhotoType
+}
+
+type AllTypes = GeneralInfoType | PhotoType | SubjectsType
+
 export type useStepContextType = () => {
-  handleStepData: (label: string, data: string) => void
-  stepData: stepDataType
+  handleStepData: (label: string, data: AllTypes) => void
+  stepData: StepDataType
 }
