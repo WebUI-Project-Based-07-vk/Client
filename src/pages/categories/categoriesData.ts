7ced91168599092ec7f7ff3e391618fd90dce39e
Halyna Prolubshchykova <galinaprolubsikova@gmail.com>
2024-08-15 08:14:10 +0300

    new commit

--- src/pages/categories/categoriesData.ts
+++ src/pages/categories/categoriesData.ts
@@ -10,89 +10,109 @@ import LegendToggleIcon from '@mui/icons-material/LegendToggle'
 import ScienceIcon from '@mui/icons-material/Science'
 import StarIcon from '@mui/icons-material/Star'
 
-export const categoriesData = [
+interface Category {
+  backgroundColor: string
+  icon: React.ElementType
+  iconColor: string
+  offers: number
+  name: string
+}
+
+export const categoriesData: Category[] = [
   {
+    id: '1',
     backgroundColor: '#dcedc8',
     icon: LanguageIcon,
     iconColor: '#aed581',
     offers: 234,
-    title: 'Languages'
+    name: 'Languages'
   },
   {
+    id: '2',
     backgroundColor: '#fff3e0',
     icon: NumbersIcon,
     iconColor: '#ffcc80',
     offers: 234,
-    title: 'Mathematics'
+    name: 'Mathematics'
   },
   {
+    id: '3',
     backgroundColor: '#e0e0e0',
     icon: TvIcon,
     iconColor: '#757575',
     offers: 234,
-    title: 'Computer Science'
+    name: 'Computer Science'
   },
   {
+    id: '4',
     backgroundColor: '#fce4ec',
     icon: MusicNoteIcon,
     iconColor: '#f06292',
     offers: 234,
-    title: 'Music'
+    name: 'Music'
   },
   {
+    id: '5',
     backgroundColor: '#e0f7fa',
     icon: DesignServicesIcon,
     iconColor: '#4dd0e1',
     offers: 234,
-    title: 'Design'
+    name: 'Design'
   },
   {
+    id: '6',
     backgroundColor: '#ffebee',
     icon: LanguageIcon,
     iconColor: '#ef5350',
     offers: 234,
-    title: 'History'
+    name: 'History'
   },
   {
+    id: '7',
     backgroundColor: '#e0e0e0',
     icon: BiotechIcon,
     iconColor: '#757575',
     offers: 234,
-    title: 'Biology'
+    name: 'Biology'
   },
   {
+    id: '8',
     backgroundColor: '#dcedc8',
     icon: PaletteIcon,
     iconColor: '#aed581',
     offers: 234,
-    title: 'Painting'
+    name: 'Painting'
   },
   {
+    id: '9',
     backgroundColor: '#fff3e0',
     icon: AccountBalanceIcon,
     iconColor: '#ffcc80',
     offers: 234,
-    title: 'Finances'
+    name: 'Finances'
   },
   {
+    id: '10',
     backgroundColor: '#ffebee',
     icon: LegendToggleIcon,
     iconColor: '#ef5350',
     offers: 234,
-    title: 'Audit'
+    name: 'Audit'
   },
   {
+    id: '11',
     backgroundColor: '#fce4ec',
     icon: ScienceIcon,
     iconColor: '#f06292',
     offers: 234,
-    title: 'Chemistry'
+    name: 'Chemistry'
   },
   {
+    id: '12',
     backgroundColor: '#e0f7fa',
     icon: StarIcon,
     iconColor: '#4dd0e1',
     offers: 234,
-    title: 'Astronomy'
+    name: 'Astronomy'
   }
 ]
